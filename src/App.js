30072354c0000b343832d416c7a44c36a77d5a2d
AnthonyByansi <byansianthony@gmail.com>
2022-12-17 16:27:13 +0300

    add image titles and discreptions

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ function App() {
     <div className="App">
       <Navbar />
       <AboutMe 
-        aboutMe={<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>}
+        aboutMe={<p>As a UX and UI designer, I'm passionate about creating intuitive and user-friendly designs that enhance the user experience. my career goal is to improve the way people interact with technology by designing easy-to-use interfaces. I'm constantly exploring new design techniques and staying up-to-date with the latest design trends. I am responsible for designing the user experience and user interface of digital products, such as websites and mobile apps. I create wireframes, prototypes, and high-fidelity designs, as well as conduct user research and testing to ensure that the product is intuitive, user-friendly, and visually appealing. My goal as a UX/UI designer is to create designs that meet the needs of users and improve their overall experience with the product. I believe that great design has the power to drive user engagement, conversion, and satisfaction, and I am committed to helping businesses and organizations achieve their goals through thoughtful and effective design. If you have a project that you think would benefit from my UX/UI design skills, please don't hesitate to get in touch. I would love to discuss your goals and ideas, and help you bring your vision to life. Thank you for considering me for your design needs.</p>}
       />
       <h2>Projects</h2>
       <div className="project-grid">
         <ProjectCard 
-          image={require('./images/project1.jpg')}
-          title="Project 1"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          image={require('./images/L.png')}
+          title="Login and SignUp "
+          description="A login and signup page is a crucial part of any website or application, as it allows users to create an account and access the site's features and content"
         />
         <ProjectCard 
-          image={require('./images/project2.jpg')}
-          title="Project 2"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          image={require('./images/frame1.jpg')}
+          title="Addax Enterprise"
+          description="Ugandan based Website design for service delivery"
         />
         <ProjectCard 
-          image={require('./images/project3.jpg')}
-          title="Project 3"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          image={require('./images/christmas.png')}
+          title="Christmas Times"
+          description="A simple christmas flyer design"
         />
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
     <nav>
       <a href="/">Home</a>
       <a href="/about">About</a>
-      <a href="https://linkedin.com/in/your-username" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://linkedin.com/in/byansi_Anthony" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://github.com/AnthonyByansi" target="_blank" rel="noopener noreferrer">GitHub</a>
     </nav>
   );
 };
